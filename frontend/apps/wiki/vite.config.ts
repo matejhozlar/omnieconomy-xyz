a3d16ae75b81802a38ce5632369c56c73049bb5d
Matej <hozlarmatej0@gmail.com>
2025-10-27 13:16:04 +0100

    fixes

--- frontend/apps/wiki/vite.config.ts
+++ frontend/apps/wiki/vite.config.ts
@@ -5,6 +5,7 @@ import path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: path.resolve(__dirname, "../../public"),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
