4cd0c14b7324a21670ea6cdfaa4c63c436cb3033
Matej <hozlarmatej0@gmail.com>
2025-10-29 18:33:41 +0100

    removing loading screen

--- frontend/apps/client/src/App.tsx
+++ frontend/apps/client/src/App.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from "@omnieconomy/shared-components";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
-import InitialLoader from "./components/InitialLoader/InitialLoader";
-import { useEffect, useState } from "react";
+// import InitialLoader from "./components/InitialLoader/InitialLoader";
+// import { useEffect, useState } from "react";
 
 function App() {
   return (
@@ -15,38 +15,38 @@ function App() {
 }
 
 function AppWithRouter() {
-  const [showLoader, setShowLoader] = useState(true);
-  const [hasLoadedOnce, setHasLoadedOnce] = useState(false);
+  // const [showLoader, setShowLoader] = useState(true);
+  // const [hasLoadedOnce, setHasLoadedOnce] = useState(false);
 
-  useEffect(() => {
-    const hasShownLoader = sessionStorage.getItem("hasShownLoader");
+  // useEffect(() => {
+  //   const hasShownLoader = sessionStorage.getItem("hasShownLoader");
 
-    if (hasShownLoader === "true") {
-      setShowLoader(false);
-      setHasLoadedOnce(true);
-    }
-  }, []);
+  //   if (hasShownLoader === "true") {
+  //     setShowLoader(false);
+  //     setHasLoadedOnce(true);
+  //   }
+  // }, []);
 
-  const handleLoaderComplete = () => {
-    setShowLoader(false);
-    setHasLoadedOnce(true);
+  // const handleLoaderComplete = () => {
+  //   setShowLoader(false);
+  //   setHasLoadedOnce(true);
 
-    sessionStorage.setItem("hasShownLoader", "true");
-  };
+  //   sessionStorage.setItem("hasShownLoader", "true");
+  // };
 
   return (
     <>
-      {showLoader && <InitialLoader onComplete={handleLoaderComplete} />}
+      {/* {showLoader && <InitialLoader onComplete={handleLoaderComplete} />}
 
       <div
         style={{ opacity: hasLoadedOnce ? 1 : 0, transition: "opacity 0.5s" }}
-      >
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
+      > */}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      {/* </div> */}
     </>
   );
 }
