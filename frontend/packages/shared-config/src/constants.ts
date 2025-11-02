1b1931ec161a1e28a394ef41e56bbc2c61af3384
Matej <hozlarmatej0@gmail.com>
2025-11-02 16:06:34 +0100

    modrinth release

--- frontend/packages/shared-config/src/constants.ts
+++ frontend/packages/shared-config/src/constants.ts
@@ -9,7 +9,7 @@ export interface LINKS_INTERFACE {
 
 export const LINKS = {
   CURSEFORGE: "https://www.curseforge.com/minecraft/mc-mods/omnieconomy",
-  MODRINTH: "https://modrinth.com/dashboard/projects",
+  MODRINTH: "https://modrinth.com/mod/omnieconomy",
   DISCORD: "https://discord.gg/mNcm76HXFy",
   GITHUB: "https://github.com/matejhozlar/omnieconomy",
   CREATE: "https://www.curseforge.com/minecraft/mc-mods/create",
